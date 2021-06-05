83dee215c34e47cd6182965d85af93dd6db54b77
= <rankovictamaraa@gmail.com>
2021-06-05 23:11:49 +0200

    backend for search, feed and profile display

--- microservices/auth/src/main/java/app/auth/service/RegularUserService.java
+++ microservices/auth/src/main/java/app/auth/service/RegularUserService.java
@@ -1,9 +1,12 @@
 package app.auth.service;
 
+import app.auth.exception.UserNotFoundException;
 import app.auth.model.RegularUser;
+import app.auth.model.dto.UserInfoDTO;
 
 public interface RegularUserService {
     void register(RegularUser user);
     void update(RegularUser user);
     RegularUser getRegularUser(long id);
+    UserInfoDTO get(String username) throws UserNotFoundException;
 }
--- microservices/auth/src/main/resources/data-postgres.sql
+++ microservices/auth/src/main/resources/data-postgres.sql
@@ -1,3 +1,9 @@
 insert into users(id, username, password, role) values (1, 'pera', 'pera', 0);
 insert into users(id, username, password, role) values (2, 'pera3', 'pera3', 0);
+insert into users(id, username, password, role) values (3, 'mika', 'mika', 0);
+insert into users(id, username, password, role) values (4, 'zika', 'zika', 0);
 
+insert into regular_user(id, user_id, name, surname, gender, email, phone_number, birth_date, website, biography) values (1, 1, 'pera', 'peric', 0, 'pera', '', '2001-02-25 08:00:00.000000', '', 'i am pera');
+insert into regular_user(id, user_id, name, surname, gender, email, phone_number, birth_date, website, biography) values (2, 2, 'pera3', 'peric3', 0, 'pera3', '', '2001-02-25 08:00:00.000000', '', 'i am pera3');
+insert into regular_user(id, user_id, name, surname, gender, email, phone_number, birth_date, website, biography) values (3, 3, 'mika', 'mikic', 0, 'mika', '', '2001-02-25 08:00:00.000000', '', 'i am mika');
+insert into regular_user(id, user_id, name, surname, gender, email, phone_number, birth_date, website, biography) values (4, 4, 'zika', 'zikic', 0, 'zika', '', '2001-02-25 08:00:00.000000', '', 'i am zika');
