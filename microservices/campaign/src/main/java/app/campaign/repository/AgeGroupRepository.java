5b8a04b7d592f5d1c28105467617cf78f81170c7
= <rankovictamaraa@gmail.com>
2021-07-03 19:59:24 +0200

    campaign repositories created

---
+++ microservices/campaign/src/main/java/app/campaign/repository/AgeGroupRepository.java
@@ -0,0 +1,9 @@
+package app.campaign.repository;
+
+import app.campaign.model.AgeGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgeGroupRepository extends JpaRepository<AgeGroup, Long> {
+}
---
+++ microservices/campaign/src/main/java/app/campaign/repository/DetailsRepository.java
@@ -0,0 +1,9 @@
+package app.campaign.repository;
+
+import app.campaign.model.CampaignDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DetailsRepository extends JpaRepository<CampaignDetails, Long> {
+}
